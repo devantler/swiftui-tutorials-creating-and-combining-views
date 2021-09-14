c4546430e56d148d60705f71c8c44386400e792c
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2021-09-14 16:19:12 +0200

    Updated .gitignore and Package.swift

--- Package.swift
+++ Package.swift
@@ -1,13 +1,12 @@
 // swift-tools-version:5.4
 import PackageDescription
-let packageName = "Landmarks" // <-- Change this to yours
 let package = Package(
-  name: packageName,
+  name: "Landmarks",
   platforms: [.iOS(.v14), .macOS(.v11)],
   products: [
-    .library(name: packageName, targets: ["LandmarksApp"])
+    .library(name: "Landmarks", targets: ["LandmarksApp"])
   ],
   targets: [
-    .executableTarget(name: "LandmarksApp", path: packageName, exclude: ["Info.plist"], resources: [.process("Assets.xcassets")])
+    .executableTarget(name: "LandmarksApp", exclude: ["Info.plist"], resources: [.process("Assets.xcassets")])
   ]
 )
